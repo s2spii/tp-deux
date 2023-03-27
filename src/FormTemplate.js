a837db24086a7b83f1064271fddcca7bf9260a72
Rayan Al Ayoubi <ralayoubi56@gmail.com>
2023-03-27 13:00:59 +0200

    TP 2 fini

---
+++ src/FormTemplate.js
@@ -0,0 +1,38 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+export const FormTemplate = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="name" required autoComplete="off" />
+      <label>Email</label>
+      <input type="email" name="email" required autoComplete="off" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" required />
+    </form>
+  );
+};
